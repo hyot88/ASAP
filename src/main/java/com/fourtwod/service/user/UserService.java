e2d67c529cc50e37301b879b75b219ae0d151d93
hyot <allsdfgh88@gmail.com>
2022-09-16 10:07:02 +0900

    닉네임 되묻기, 자릿수 체크, 색상 변경

--- src/main/java/com/fourtwod/service/user/UserService.java
+++ src/main/java/com/fourtwod/service/user/UserService.java
@@ -21,6 +21,38 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
+    public ResponseCode updateCheck(String email, String registrationId, UserSaveRequestDto userSaveRequestDto) throws Exception {
+        User userByEmail = userRepository.findByUserId(UserId.builder().email(email).registrationId(registrationId).build()).orElse(null);
+
+        // Email이 검색되지 않을 경우
+        if (userByEmail == null) {
+            return ResponseCode.COMM_E001;
+        }
+
+        String nickname = userSaveRequestDto.getNickname();
+
+        // 2자 이상 10자 이하 닉네임이 아닌 경우
+        if (nickname.length() < 2 || nickname.length() > 10) {
+            return ResponseCode.NICK_E001;
+        }
+
+        // 한글,영문,숫자 닉네임이 아닌 경우
+        if (!Pattern.matches("^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$", nickname)) {
+            return ResponseCode.NICK_E002;
+        }
+
+        User userByNickname = userRepository.findByNickname(nickname).orElse(null);
+
+        // 닉네임이 중복될 경우
+        if (userByNickname != null) {
+            return ResponseCode.NICK_E003;
+        }
+
+        return ResponseCode.COMM_S000;
+    }
+
+    // TODO: 중복코드 제거 필요함
+    @Transactional
     public ResponseCode update(String email, String registrationId, UserSaveRequestDto userSaveRequestDto) throws Exception {
         User userByEmail = userRepository.findByUserId(UserId.builder().email(email).registrationId(registrationId).build()).orElse(null);
 
@@ -28,14 +60,14 @@ public class UserService {
         if (userByEmail == null) {
             return ResponseCode.COMM_E001;
         }
-        
+
         String nickname = userSaveRequestDto.getNickname();
 
         // 2자 이상 10자 이하 닉네임이 아닌 경우
         if (nickname.length() < 2 || nickname.length() > 10) {
             return ResponseCode.NICK_E001;
         }
-        
+
         // 한글,영문,숫자 닉네임이 아닌 경우
         if (!Pattern.matches("^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$", nickname)) {
             return ResponseCode.NICK_E002;

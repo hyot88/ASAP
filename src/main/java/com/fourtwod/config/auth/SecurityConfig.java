5fd38ca2e422fd5832ea212abb91dbfe8237595b
hyot <allsdfgh88@gmail.com>
2022-10-06 14:52:28 +0900

    profile url 추가

--- src/main/java/com/fourtwod/config/auth/SecurityConfig.java
+++ src/main/java/com/fourtwod/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/api/nmReset").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/api/nmReset", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()

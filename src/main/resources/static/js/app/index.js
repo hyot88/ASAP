1cb2024f647c27405d1c817d6f10a0415712c6db
yyounsj <yyounsj@gmail.com>
2022-11-07 23:03:21 +0900

    메인 - 진행중인 미션없을때 케이스 - 사용자 랭크 정보 api 호출

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,6 +1,45 @@
 var main = {
-    init : function () {},
-    proc : function () {}
+    init : function () {
+        var _this = this;
+
+        _this.getRankInfo()
+    },
+    proc : function () {
+    },
+    getRankInfo: function () {
+        $.ajax({
+            type: 'GET',
+            url: '/api/user/rank/0',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+        }).done(function(response) {
+            if (response.code == 0) {
+                console.log(response)
+                var info = response.data[0];
+
+                var template = $('#rankBox-template').html();
+                template = template.replaceAll('{', '{{').replaceAll('}', '}}')
+                Mustache.parse(template);
+
+                var data = {
+                	 ranking: info.ranking || '',
+                     tier: info.tier || 0,
+                     tierPoint: info.tierPoint || 0,
+                     nickname: info.nickname || ''
+                };
+
+                var rendered = Mustache.render(template, data);
+                $('#rankBox').html(rendered);
+
+                $('#rankBox').removeClass('hide')
+            } else {
+                _this.util.alertMessage(response.message);
+            }
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+
+    }
 };
 
 main.init();

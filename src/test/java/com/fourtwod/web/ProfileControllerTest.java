e542a43dea8eb9684833356679357b8e4fe67c07
2020021 <hsahn0928@interpark.com>
2022-11-07 17:18:18 +0900

    미션 api

--- src/test/java/com/fourtwod/web/ProfileControllerTest.java
+++ src/test/java/com/fourtwod/web/ProfileControllerTest.java
@@ -28,4 +28,10 @@ public class ProfileControllerTest {
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getBody()).isEqualTo(expected);
     }
+
+    @Test
+    public void TierInfo테스트() throws Exception {
+        int userTier = 9;
+        int userTierPoint = 956;
+    }
 }

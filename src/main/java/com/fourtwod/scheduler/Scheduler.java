0e03504475ece1646b727817de35d2a004699d9c
2020021 <hsahn0928@interpark.com>
2022-11-09 17:25:46 +0900

    스케줄러 추가

---
+++ src/main/java/com/fourtwod/scheduler/Scheduler.java
@@ -0,0 +1,25 @@
+package com.fourtwod.scheduler;
+
+import com.fourtwod.scheduler.facade.SchedulerFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Scheduler {
+
+    private final SchedulerFacade schedulerFacade;
+
+    // 매일 0시 0분 0초
+    @Scheduled(cron = "0 0 0 * * *")
+    public void dailySchedule() {
+        schedulerFacade.dailySchedule();
+    }
+
+    // 매달 1일 0시 30분 0초
+    @Scheduled(cron = "0 0 0 1 * *")
+    public void monthSchedule() {
+        schedulerFacade.monthSchedule();
+    }
+}

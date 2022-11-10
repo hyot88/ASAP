40f801e336a4dbeb57827150cf52fb991dab81e6
hyot <allsdfgh88@gmail.com>
2022-11-10 19:05:46 +0900

    스크립트 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
+ABSDIR=$(dirname $ABSPATH)/${PROJECT_NAME}/scripts
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {

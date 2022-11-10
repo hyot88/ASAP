2000378ce6741c2d14a6721d467779bcbf6a2c53
yyounsj <yyounsj@gmail.com>
2022-11-10 13:57:44 +0900

    메인 - 진행중인 미션없을때 케이스 - 미션생성 api 연동

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,6 +3,7 @@ var main = {
         var _this = this;
 
         _this.getRankInfo()
+        _this.getMission()
     },
     proc : function () {
     },
@@ -38,7 +39,60 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+    getMission: function () {
+        $.ajax({
+            type: 'GET',
+            url: '/api/mission',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+        }).done(function(response) {
+            if (response.code == 0) {
+                console.log(response)
+                var info = response.data.detail || [];
+                var template = '';
+                var data = {};
 
+                if (info.length > 0) {
+                    template = $('#progressMissionBox-template').html();
+                } else {
+                    template = $('#noProgressMissionBox-template').html();
+                }
+                Mustache.parse(template);
+                var rendered = Mustache.render(template, data);
+                $('#missionBox').html(rendered);
+
+                $('#missionBox').removeClass('hide')
+
+                $('.mission_day').click(function (e) {
+                    e.preventDefault()
+                    main.setMission($(this).attr('data-day'))
+                })
+            } else {
+                _this.util.alertMessage(response.message);
+            }
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    setMission: function (missionType) {
+        if (missionType !== undefined && confirm(missionType + 'day Mission을 시작 하시겠습니까?')) {
+            $.ajax({
+                type: 'POST',
+                url: 'api/mission/' + missionType,
+                dataType: 'json',
+                contentType:'application/json; charset=utf-8',
+            }).done(function(response) {
+                if (response.code == 0) {
+                    console.log(response)
+                    main.getMission();
+                } else {
+                    _this.util.alertMessage(response.message);
+                }
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        }
     }
 };
 
